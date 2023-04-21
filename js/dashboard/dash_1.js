863345d795c0c30793645575d52ec1f19c7cad42
Ayka11 <32898816+Ayka11@users.noreply.github.com>
2023-04-21 10:47:09 +0400

    public2

---
+++ js/dashboard/dash_1.js
@@ -0,0 +1,15 @@
+
+  /*
+      =============================================
+          Perfect Scrollbar | Recent Activities
+      =============================================
+  */
+  const ps = new PerfectScrollbar(document.querySelector('.mt-container'));
+  //notifications scroll
+  const ns = new PerfectScrollbar(document.querySelector('.notification-scroll'));
+  //transactions scroll
+  const ts = new PerfectScrollbar(document.querySelector('.widget-table-one .widget-content'));
+  //active trades
+  const at = new PerfectScrollbar(document.querySelector('.widget-table-two .widget-content'));
+  at.scrollTop=0
+
